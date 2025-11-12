41bae4ad93d78371954dd961b56a2847599b6827
luzai <1923447102@qq.com>
2025-11-12 16:32:10 +0800

    fix: 前端完成了航班查询

---
+++ aircraft-server/src/main/java/cn/luzai/aircraftserver/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package cn.luzai.aircraftserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080"); // 替换为前端实际访问地址
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
